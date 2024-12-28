2575e2be49f655eec15eedc30127fa2d890dc85c
defk0n1 <zrigua.ghassen@gmail.com>
2024-12-28 22:44:36 +0100

    eslint rules for deployment

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig: NextConfig = {
    images: {
     domains: ['res.cloudinary.com', "m.media-amazon.com"], // Add Cloudinary's domain here
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
   /* config options here */
 };
 
